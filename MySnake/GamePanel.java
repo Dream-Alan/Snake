534c87d3283e000019dc8423ddf63af507c6c52b
Dream-Alan <805292940@qq.com>
2026-02-25 19:41:33 +0800

    创建了贪吃蛇相关游戏文件,并初始化了地图

--- MySnake/GamePanel.java
+++ MySnake/GamePanel.java
@@ -1,5 +1,67 @@
 package MySnake;
 import java.awt.*;
 import javax.swing.*;
-public class GamePanel {
+public class GamePanel extends JFrame {
+    static final int CELL_SIZE = 20;
+    static final int ROW = 22;
+    static final int COL = 32;
+    public GamePanel(){
+        GameFrame();
+
+    }
+
+
+    public void GameFrame() {
+        new JFrame();
+        setSize(1000, 800);
+        setTitle("Snake");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLayout(null);
+
+        init();
+    }
+    public void init(){
+        GameMap gameMap = new GameMap();
+        this.add(gameMap,TOP_ALIGNMENT);
+    }
+}
+class GameMap extends JPanel{
+    public GameMap(){
+        new JPanel();
+        setBackground(Color.GRAY);
+        setSize(GamePanel.CELL_SIZE*GamePanel.COL,GamePanel.CELL_SIZE*GamePanel.ROW);
+
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(Color.WHITE);
+        for(int c=0;c<GamePanel.COL;c++){
+            for(int r=0;r<GamePanel.ROW;r++){
+                int x=c*GamePanel.CELL_SIZE;
+                int y=r*GamePanel.CELL_SIZE;
+
+                g.drawRect(x,y,GamePanel.CELL_SIZE,GamePanel.CELL_SIZE);
+            }
+        }
+        for(int c=0;c<GamePanel.COL;c++){
+            int x=c*GamePanel.CELL_SIZE;
+            g.drawString("*",x+10,15);
+        }
+        for(int r=0;r<GamePanel.ROW;r++){
+            int y=r*GamePanel.CELL_SIZE;
+            g.drawString("*",10,y+15);
+        }
+        for(int c=0;c<GamePanel.COL;c++){
+            int x=c*GamePanel.CELL_SIZE;
+            g.drawString("*",x+15,22*GamePanel.CELL_SIZE-5);
+        }
+        for(int r=0;r<GamePanel.ROW;r++){
+            int y=r*GamePanel.CELL_SIZE;
+            g.drawString("*",32*GamePanel.CELL_SIZE-15,y+15);
+        }
+    }
 }
--- MySnake/Snake.java
+++ MySnake/Snake.java
@@ -3,4 +3,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 public class Snake {
+    public static void main(String[] args) {
+        new GamePanel();
+
+    }
 }
